1d04703c5ceee70a51459fbba8ff4567446d34f8
agustinettig <40875912+agustinettig@users.noreply.github.com>
2020-11-06 23:35:22 -0300

    initial version

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,37 @@
+import { startOfHour } from 'date-fns';
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface Request {
+    provider: string;
+    date: Date;
+}
+
+class CreateAppointmentService {
+    private appointmentsRepository: AppointmentsRepository;
+
+    constructor(appointmentsRepository: AppointmentsRepository) {
+        this.appointmentsRepository = appointmentsRepository;
+    }
+
+    public execute({ provider, date }: Request): Appointment {
+        const appointmentDate = startOfHour(date);
+
+        const appointmentOnSameTime = this.appointmentsRepository.findByDate(
+            appointmentDate,
+        );
+
+        if (appointmentOnSameTime) {
+            throw Error('There is already a booked appointment');
+        }
+
+        const appointment = this.appointmentsRepository.create({
+            provider,
+            date: appointmentDate,
+        });
+
+        return appointment;
+    }
+}
+
+export default CreateAppointmentService;
