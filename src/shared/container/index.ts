ae249d5352b9c2457568ccc0aa672a1d11851cf3
agustinettig <40875912+agustinettig@users.noreply.github.com>
2020-11-28 18:22:19 -0300

    add password routes

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -9,5 +9,9 @@ import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepo
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
 container.registerSingleton<IAppointmentsRepository>('AppointmentsRepository', AppointmentsRepository);
+container.registerSingleton<IUserTokensRepository>('UserTokensRepository', UserTokensRepository);
